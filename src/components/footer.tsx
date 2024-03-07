ace784d4ee785c47e07bf27773345ae91afc3506
vuhoaian <vuhoaian1804@gmail.com>
2024-03-07 15:47:50 +0700

    b1

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+const Footer = ()=>{
+    return (
+        <div className="p-3 mb-2 bg-warning text-dark text-center">Footer</div>
+    )
+}
+export default Footer
---
+++ src/components/products.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+
+
+function Products() {
+  return (
+   <>
+   <div className="row">
+  <div className="col-sm-3">
+    <div className="card">
+    <img className="card-img-top" src="https://vtv1.mediacdn.vn/2019/10/10/photo-1-15706463929181755249740.jpg" alt="Card image cap"/>
+
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-3">
+    
+    <div className="card">
+    <img className="card-img-top" src="https://vtv1.mediacdn.vn/2019/10/10/photo-1-15706463929181755249740.jpg" alt="Card image cap"/>
+
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-3">
+    <div className="card">
+    <img className="card-img-top" src="https://vtv1.mediacdn.vn/2019/10/10/photo-1-15706463929181755249740.jpg" alt="Card image cap"/>
+
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-3">
+    <div className="card">
+    <img className="card-img-top" src="https://vtv1.mediacdn.vn/2019/10/10/photo-1-15706463929181755249740.jpg" alt="Card image cap"/>
+
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-3">
+    <div className="card">
+    <img className="card-img-top" src="https://vtv1.mediacdn.vn/2019/10/10/photo-1-15706463929181755249740.jpg" alt="Card image cap"/>
+
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-3">
+    <div className="card">
+    <img className="card-img-top" src="https://vtv1.mediacdn.vn/2019/10/10/photo-1-15706463929181755249740.jpg" alt="Card image cap"/>
+
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-3">
+    <div className="card">
+    <img className="card-img-top" src="https://vtv1.mediacdn.vn/2019/10/10/photo-1-15706463929181755249740.jpg" alt="Card image cap"/>
+
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-3">
+    <div className="card">
+    <img className="card-img-top" src="https://vtv1.mediacdn.vn/2019/10/10/photo-1-15706463929181755249740.jpg" alt="Card image cap"/>
+
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+  
+  
+
+
+
+   </>
+  );
+}
+
+export default Products;
